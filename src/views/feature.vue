31163b3b5475711a0d5f9cb7132e1c436dae74de
Mohana Kandula <mohanak@mkcl.org>
2020-07-13 11:36:03 +0530

    new feature

---
+++ src/views/feature.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+      <div class="col">
+          <div class="row">
+              <div class="col">
+                  
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
